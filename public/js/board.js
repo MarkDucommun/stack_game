b357173b1092c984ebeec98e56edb86fe31de4d6
Mark E. Ducommun <ducommun.mark@gmail.com>
2013-08-24 00:17:11 -0500

    trying to disable rubberband scrolling

--- public/js/board.js
+++ public/js/board.js
@@ -37,7 +37,19 @@ Board.prototype.startPlay = function(){
     board.isGameFinished()
   })
 
-  $(document).on('touchmove', false)
+  document.body.addEventListener('touchmove', function(event) {
+      console.log(event.source);
+      //if (event.source == document.body)
+        event.preventDefault();
+    }, false);
+ 
+    window.onresize = function() {
+      $(document.body).width(window.innerWidth).height(window.innerHeight);
+    }
+ 
+    $(function() {
+      window.onresize();
+    });
 
   $(document).swipe({
     swipe:function(event, direction){
